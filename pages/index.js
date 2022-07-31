f7e045faf2c3a8fc2fde06d01eb4cd22c4c6a9f5
蓑田朋未 <minodatomomi@minodatomominoMacBook-Air.local>
2022-07-31 18:26:05 +0900

    [add]layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Hero from 'components/hero'
+import Layout from 'components/layout'
+
 export default function Home() {
   return (
-    <div>
-      <h1>CUBE</h1>
-    </div>
-  );
+    <>
+      <Layout>
+        <Hero />
+      </Layout>
+    </>
+  )
 }
