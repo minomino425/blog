d1a7017635b7e9e978db4c00bb34074b41698f9a
蓑田朋未 <tomomi.minoda425@gmail.com>
2022-08-05 23:50:56 +0900

    [add]fotter.css

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import styles from 'styles/footer.module.css'
 
 export default function Footer() {
   return (
-    <footer className={styles.flexContainer}>
+    <footer>
       <Logo />
     </footer>
   )
