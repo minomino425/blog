68503d8b3fc5b04f86bbadcdebdcf02223307ec3
蓑田朋未 <tomomi.minoda425@gmail.com>
2022-08-14 11:12:45 +0900

    [add]getStaticPost

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
+import { getAllPosts } from 'lib/api'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Meta from 'components/meta'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import Posts from 'components/post'
+import Pagination from 'components/pagination'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <>
       <Container>
         <Meta />
         <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
+        <Posts posts={posts} />
+        <Pagination nextUrl='/blog' nextText='MorePost'/>
       </Container>
     </>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
