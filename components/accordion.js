71bc31e1fde4c47d04c3f85c9195a1cba40a35d0
蓑田朋未 <tomomi.minoda425@gmail.com>
2022-08-16 08:03:54 +0900

    [add]accordion.js

---
+++ components/accordion.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import styles from 'styles/accordion.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+export default function Accordion({ heading, children }) {
+  const [textIsOpen, setTextIsOpen] = useState(false)
+
+  const toggleText = () => {
+    setTextIsOpen((prev) => !prev)
+  }
+
+  return (
+    <div className={textIsOpen ? styles.open : styles.close}>
+      <h3 className={styles.heading}>
+        <button onClick={toggleText}>
+          {heading}
+          <FontAwesomeIcon
+            icon={faCircleChevronDown}
+            className={styles.icon}
+          />
+        </button>
+      </h3>
+      <div className={styles.text}>
+        <div className={styles.textInner}>{children}</div>
+      </div>
+    </div>
+  )
+}
