780f0faf5be6a28bdf921e02dd65c534f8b6caa0
蓑田朋未 <tomomi.minoda425@gmail.com>
2022-08-17 00:18:12 +0900

    [add]useRef

--- components/accordion.js
+++ components/accordion.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import styles from 'styles/accordion.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons'
@@ -10,18 +10,25 @@ export default function Accordion({ heading, children }) {
     setTextIsOpen((prev) => !prev)
   }
 
+  const refText = useRef(null)
+
   return (
     <div className={textIsOpen ? styles.open : styles.close}>
       <h3 className={styles.heading}>
         <button onClick={toggleText}>
           {heading}
-          <FontAwesomeIcon
-            icon={faCircleChevronDown}
-            className={styles.icon}
-          />
+          <FontAwesomeIcon icon={faCircleChevronDown} className={styles.icon} />
         </button>
       </h3>
-      <div className={styles.text}>
+      <div
+        className={styles.text}
+        ref={refText}
+        style={{
+          '--text-height': refText.current
+            ? `${refText.current.scrollHeight}px`
+            : '0px',
+        }}
+      >
         <div className={styles.textInner}>{children}</div>
       </div>
     </div>
